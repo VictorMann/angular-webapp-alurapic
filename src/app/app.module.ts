512662b7b90e7ce102ef402cf241bc1c67295ee9
Victor Mann <victorcmann@gmail.com>
2018-10-30 19:45:06 -0200

    criando photos.module e importando em app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { PhotosModule } from './photos/photos.module';
 
-import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
