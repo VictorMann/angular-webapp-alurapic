e8de8bad7e662456b55988fb693de9fb63cb8a83
Victor Mann <victorcmann@gmail.com>
2018-10-30 18:48:01 -0200

    criando photo.component e seu template

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ap-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent
+{
+    url = 'https://meusanimais.com.br/wp-content/uploads/2018/02/passaro-colorido-1.jpg';
+    description = 'Calopsita';
+}
