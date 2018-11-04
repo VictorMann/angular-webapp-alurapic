8b0411819009d426c882145215b1d6eecc26f895
Victor Mann <victorcmann@gmail.com>
2018-11-04 18:07:17 -0200

    criando logica de signin.component

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/auth.service';
 
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SigninComponent {}
+export class SigninComponent  implements OnInit
+{
+    loginForm: FormGroup;
+
+    constructor (
+        private formBuilder: FormBuilder,
+        private authService: AuthService
+    ) {}
+
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+    }
+
+    login ()
+    {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('password').value;
+
+        this.authService
+            .authenticate(userName, password)
+            .subscribe(
+                () => console.log('autenticado'),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                }
+            );
+    }
+
+}
