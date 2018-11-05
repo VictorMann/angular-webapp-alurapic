0c0fc4057a6d09e3865c1d6a4621e99ecb150bef
Victor Mann <victorcmann@gmail.com>
2018-11-05 17:10:47 -0200

    exportando RouterModule

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,12 @@
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [HeaderComponent],
-    exports: [HeaderComponent]
+    exports: [
+        HeaderComponent,
+        RouterModule
+    ]
 })
 export class CoreModule {}
