75e4d98e9fb7e0a13ed0050f66eb042001ded975
Victor Mann <victorcmann@gmail.com>
2018-11-05 16:39:34 -0200

    definindo funcionalidade para obtencao de login/logout e estabelecendo o user logado

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
+import { UserService } from '../user/user.service';
+import { User } from '../user/user';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'ap-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent 
+{
+    user$: Observable<User>;
+    user: User;
+
+    constructor (
+        private userService: UserService,
+        private router: Router) 
+    {
+        this.user$ = userService.getUser();
+        this.user$.subscribe(user => this.user = user);
+    }
+
+    logout ()
+    {
+        this.userService.logout();
+        this.router.navigate(['']);
+    }
+}
