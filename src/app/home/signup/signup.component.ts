58578d641bb3b08387cdf41049d196c17d1a754a
Victor Mann <victorcmann@gmail.com>
2018-11-06 15:54:45 -0200

    obtendo o foco do campo email

--- src/app/home/signup/signup.component.ts
+++ src/app/home/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -6,6 +6,7 @@ import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validat
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
 import { NewUser } from './new-user';
 import { SignService } from './signup.service';
+import { PlatformDetectorService } from 'src/app/core/platform-detector/platform-detector.service';
 
 @Component({
     templateUrl: './signup.component.html'
@@ -13,12 +14,14 @@ import { SignService } from './signup.service';
 export class SignUpComponent implements OnInit
 {
     signupForm: FormGroup;
+    @ViewChild('emailInput') emailInput: ElementRef<HTMLInputElement>;
     
     constructor (
         private formBuilder: FormBuilder,
         private userNotTakenValidatorService: UserNotTakenValidatorService,
         private signUpService: SignService,
-        private router: Router
+        private router: Router,
+        private platformDetectorService: PlatformDetectorService
     ) {}
 
     ngOnInit(): void 
@@ -47,6 +50,9 @@ export class SignUpComponent implements OnInit
                 Validators.maxLength(14),
             ]]
         });
+
+        this.platformDetectorService.isPlatformBrowser() &&
+        this.emailInput.nativeElement.focus();
     }
 
     signup ()
