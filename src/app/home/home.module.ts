fbd841e058b5386225efecae55f2788d0689b4ba
Victor Mann <victorcmann@gmail.com>
2018-11-06 14:04:30 -0200

    definido rota signup e declarando em home.module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { SigninComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations: [ SigninComponent ],
+    declarations: [ 
+        SigninComponent,
+        SignUpComponent
+    ],
     imports: [ 
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMessageModule,
         RouterModule
