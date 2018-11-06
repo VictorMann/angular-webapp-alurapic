568bfbaff95f68547f8a91abf1b790baf0e8ab41
Victor Mann <victorcmann@gmail.com>
2018-11-06 16:54:25 -0200

    definindo provider para interceptar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RequestInterceptor } from './auth/request.interceptor';
 
 @NgModule({
     declarations: [HeaderComponent],
     exports: [
         HeaderComponent,
         RouterModule
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: RequestInterceptor,
+            multi: true
+        }
     ]
 })
 export class CoreModule {}
