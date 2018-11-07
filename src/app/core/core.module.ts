b95c423a1b2782ed83c980dd943fee730275859a
Victor Mann <victorcmann@gmail.com>
2018-11-07 14:19:46 -0200

    importando FooterComponent e melhorando a ordem das importacoes

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,24 @@
-import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { RequestInterceptor } from './auth/request.interceptor';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [HeaderComponent],
+    declarations: [
+        HeaderComponent,
+        FooterComponent
+    ],
+    imports: [
+        CommonModule
+    ],
     exports: [
         HeaderComponent,
-        RouterModule
+        RouterModule,
+        FooterComponent
     ],
     providers: [
         {
