a2cfd63fdf27065482e546c461230827b8cf57d9
Victor Mann <victorcmann@gmail.com>
2018-11-07 15:27:15 -0200

    corrigindo exibicao da imagem

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,13 +1,22 @@
 import { Component, Input } from "@angular/core";
 
+const CLOUD = 'http://localhost:3000/imgs/';
+
 @Component({
     selector: 'ap-photo',
     templateUrl: 'photo.component.html'
 })
 export class PhotoComponent
 {
-    @Input() url = '';
-    @Input() description = '';
+    private _url: string;
 
-    
+    @Input() description = '';
+    @Input() set url(url: string) {
+        this._url = url.startsWith('data')
+        ? url
+        : CLOUD + url;
+    };
+    get url () {
+        return this._url;
+    }
 }
