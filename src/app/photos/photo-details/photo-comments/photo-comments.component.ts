6b6928d67730f637db06dfb7a732dc643e493b31
Victor Mann <victorcmann@gmail.com>
2018-11-08 17:07:24 -0200

    validando formulario e definindo metodo de salvamento de comentario

--- src/app/photos/photo-details/photo-comments/photo-comments.component.ts
+++ src/app/photos/photo-details/photo-comments/photo-comments.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+
 import { PhotoService } from '../../photo/photo.service';
 import { PhotoComment } from '../../photo/photo-comment';
 
@@ -10,13 +13,30 @@ import { PhotoComment } from '../../photo/photo-comment';
 export class PhotoCommentsComponent implements OnInit
 {
     @Input() photoId: number;
+    commentForm: FormGroup;
 
     comments$: Observable<PhotoComment[]>;
 
-    constructor (private photoService: PhotoService) {}
+    constructor (
+        private photoService: PhotoService,
+        private formBuilder: FormBuilder
+    ) {}
 
     ngOnInit (): void
     {
         this.comments$ = this.photoService.getComments(this.photoId);
+        this.commentForm = this.formBuilder.group({
+            comment: ['', Validators.maxLength(300)]
+        });
+    }
+
+    save ()
+    {
+        const comment = this.commentForm.get('comment').value as string;
+        this.comments$ = this.photoService
+        .addComment(this.photoId, comment)
+        .pipe(switchMap(() => this.photoService.getComments(this.photoId)))
+        .pipe(tap(() => this.commentForm.reset()));
+        
     }
 }
