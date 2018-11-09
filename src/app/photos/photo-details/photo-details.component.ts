38baaf4e1b2b891ae8b16d6ace6b5c6b49cfc22f
Victor Mann <victorcmann@gmail.com>
2018-11-09 15:21:27 -0200

    redirecionando para rota not-found qnd imagem nao encontrada e definindo bool nos alerts

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { PhotoService } from '../photo/photo.service';
 import { Photo } from '../photo/photo';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
+import { UserService } from 'src/app/core/user/user.service';
 
 @Component({
     templateUrl: './photo-details.component.html'
@@ -19,7 +20,8 @@ export class PhotoDetailsComponent implements OnInit
         private route: ActivatedRoute,
         private photoService: PhotoService,
         private router: Router,
-        private alertService: AlertService
+        private alertService: AlertService,
+        private userService: UserService
     ) {}
 
     ngOnInit (): void
@@ -27,6 +29,10 @@ export class PhotoDetailsComponent implements OnInit
         this.photoId = this.route.snapshot.params.photoId;
         this.photo$ = this.photoService.findById(this.photoId);
         // this.photoService.findById(id).subscribe(photo => this.photo = photo);
+        this.photo$.subscribe(() => {}, err => {
+            console.log(err);
+            this.router.navigate(['not-found']);
+        });
     }
 
     remove ()
@@ -35,12 +41,12 @@ export class PhotoDetailsComponent implements OnInit
             .removePhoto(this.photoId)
             .subscribe(
                 () => {
-                    this.alertService.success('Photo removed');
-                    this.router.navigate(['']);
+                    this.alertService.success('Photo removed', true);
+                    this.router.navigate(['/user', this.userService.getUserName()]);
                 },
                 err => {
                     console.log(err);
-                    this.alertService.warning('Could not delete the photo!');
+                    this.alertService.warning('Could not delete the photo!', true);
                 }
             );
     }
