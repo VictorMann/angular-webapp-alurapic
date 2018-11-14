75e7e3532c6e0c41175d4da1b489be51740a28ca
Victor Mann <victorcmann@gmail.com>
2018-11-14 14:35:54 -0200

    removendo o providers GlobalErrorHandler

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -1,18 +1,11 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [NotFoundComponent],
-  providers: [
-    {
-      provide: ErrorHandler,
-      useClass: GlobalErrorHandler
-    }
-  ]
 })
 export class ErrorsModule { }
